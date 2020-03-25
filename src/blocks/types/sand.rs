e6265c1a9bcddfe3ef07327b28afd65a5545483c
municorn <municorn@musicaloft.com>
2020-03-25 00:39:33 -0600

    translated block types

---
+++ src/blocks/types/sand.rs
@@ -0,0 +1,20 @@
+/// SandBlock is a block of sand, easy to destroy/collect by
+/// hand
+struct SandBlock {
+    lifespan: f32, 
+    health f32,
+}
+
+impl SandBlock {
+    /// Returns a new SandBlock
+    fn new() -> Sel {
+        Sself {
+            lifespan: 5,
+            health: 5,
+        }
+    }
+}
+
+impl Block for SandBlock {
+
+}
