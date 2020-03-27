1093838bb49915233117d567feb819560442fbea
municorn <municorn@musicaloft.com>
2020-03-26 23:09:29 -0600

    removed input module

--- src/input/keyboard.go
+++
@@ -1,8 +0,0 @@
-// KeyboardListener listens for keyboard input.
-// Its functions are called from glfw's callback.
-trait KeyboardListener {
-	KeyDown(key glfw.Key, scancode i32, mods glfw.ModifierKey)
-	KeyUp(key glfw.Key, scancode i32, mods glfw.ModifierKey)
-	KeyRepeat(key glfw.Key, scancode i32, mods glfw.ModifierKey)
-}
-
--- src/input/mouse.go
+++
@@ -1,7 +0,0 @@
-// MouseListener listens for mouse input. Its functions are
-// called from glfw's callback.
-trait MouseListener {
-	MouseMove()
-	MouseClick()
-}
-
