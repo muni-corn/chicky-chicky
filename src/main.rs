b37e06b4ef42033cb6a436a18e1a1474f28cd5e1
municorn <municorn@musicaloft.com>
2020-04-16 12:02:48 -0600

    moving to wgpu instead of webgl

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![warn(missing_docs)]
+#![deny(unused_variables)]
+#![deny(clippy::shadow_unrelated)]
+
+mod blocks;
+mod characters;
+mod engine;
+mod game;
+mod items;
+mod maths;
+mod render;
+mod sprite;
+mod textures;
+mod utils;
+mod world;
+mod traits;
+
+fn main() {
+
+}
