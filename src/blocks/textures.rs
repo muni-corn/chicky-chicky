f47e1b29ad4e92709acf69ffe010c8872aadc690
municorn <municorn@musicaloft.com>
2020-05-03 13:28:02 -0600

    added structs for block textures
    - BlockTexture, an individual block texture
    - BlockTextures, a texture pack, basically

--- src/blocks/textures.rs
+++ src/blocks/textures.rs
@@ -1,9 +1,95 @@
-use textures;
+use crate::engine;
 
-const base_dir: &'static str = "./assets/photos/blocks/";
+pub struct BlockTexture {
+    texture: engine::Texture,
+    bind_group: wgpu::BindGroup,
+}
 
-// TODO These should be specific to their block modules
-let dirt_texture = textures::new_lazy_texture("dirt.png")
-let grass_texture = textures::new_lazy_texture("grass.png")
-let stone_texture = textures::new_lazy_texture("stone.png")
-let sand_texture = textures::new_lazy_texture("sand.png")
+impl BlockTexture {
+    fn new(
+        device: &wgpu::Device,
+        bind_group_layout: &wgpu::BindGroupLayout,
+        bytes: &[u8],
+    ) -> Result<(Self, wgpu::CommandBuffer), Box<dyn std::error::Error>> {
+        let (texture, cmd) = engine::Texture::from_bytes(device, bytes)?;
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: bind_group_layout,
+            bindings: &[
+                wgpu::Binding {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&texture.view),
+                },
+                wgpu::Binding {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&texture.sampler),
+                },
+            ],
+        });
+
+        Ok((
+            Self {
+                texture,
+                bind_group,
+            },
+            cmd,
+        ))
+    }
+
+    pub fn get_bind_group(&self) -> &wgpu::BindGroup {
+        &self.bind_group
+    }
+}
+
+pub struct BlockTextures {
+    pub dirt: BlockTexture,
+    pub stone: BlockTexture,
+    pub grass: BlockTexture,
+    pub sand: BlockTexture,
+}
+
+impl BlockTextures {
+    pub fn default(
+        device: &wgpu::Device,
+        block_texture_bind_group_layout: &wgpu::BindGroupLayout,
+    ) -> (Self, Vec<wgpu::CommandBuffer>) {
+        let (dirt, dirt_cmd) = BlockTexture::new(
+            device,
+            block_texture_bind_group_layout,
+            include_bytes!("../../assets/images/blocks/dirt.png"),
+        )
+        .unwrap();
+
+        let (stone, stone_cmd) = BlockTexture::new(
+            device,
+            block_texture_bind_group_layout,
+            include_bytes!("../../assets/images/blocks/stone.png"),
+        )
+        .unwrap();
+
+        let (grass, grass_cmd) = BlockTexture::new(
+            device,
+            block_texture_bind_group_layout,
+            include_bytes!("../../assets/images/blocks/grass.png"),
+        )
+        .unwrap();
+
+        let (sand, sand_cmd) = BlockTexture::new(
+            device,
+            block_texture_bind_group_layout,
+            include_bytes!("../../assets/images/blocks/sand.png"),
+        )
+        .unwrap();
+
+        let commands = vec![dirt_cmd, stone_cmd, grass_cmd, sand_cmd];
+
+        (
+            Self {
+                dirt,
+                stone,
+                grass,
+                sand,
+            },
+            commands,
+        )
+    }
+}
